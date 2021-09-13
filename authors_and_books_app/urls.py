8e49877cdca31b0def0426e938ff71892882bc14
Ciara Giron <cjgiron96@gmail.com>
2021-09-13 15:09:55 -0700

    added display pages and authors functionality

--- authors_and_books_app/urls.py
+++ authors_and_books_app/urls.py
@@ -5,5 +5,12 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('process_book', views.process_book),
-    path('books/<int:book_id>/delete', views.delete),   
+    path('books/<int:book_id>/delete', views.delete), 
+    path('books/<int:book_id>/display', views.display),  
+    path('add_author', views.add_author),
+    path('authors', views.authors),
+    path('process_author', views.process_author),
+    path('authors/<int:author_id>/delete', views.delete_author),
+    path('authors/<int:author_id>/display', views.display_author),
+    path('add_book', views.add_book),
 ]
--- authors_and_books_app/views.py
+++ authors_and_books_app/views.py
@@ -22,4 +22,78 @@ def delete(request, book_id):
     this_book = Book.objects.get(id=book_id)
     this_book.delete()
 
-    return redirect('/') 
+    return redirect('/')
+
+
+def delete_author(request, author_id):
+    this_author = Author.objects.get(id=author_id)
+    this_author.delete()
+
+    return redirect('/authors')
+
+
+def display(request, book_id):
+    this_book = Book.objects.get(id=book_id)
+
+    context = {
+        'book': this_book,
+        'authors': Author.objects.all(),
+    }
+
+    return render(request, 'display_book.html', context)
+
+
+def display_author(request, author_id):
+    this_author = Author.objects.get(id=author_id)
+
+    context = {
+        'author': this_author,
+        'books': Book.objects.all()
+    }
+
+    return render(request, 'display_author.html', context)
+
+def add_author(request):
+    print(request.POST)
+
+    author_id = request.POST['author']
+    book_id = request.POST['book_id']
+    author = Author.objects.get(id=author_id)
+    book = Book.objects.get(id=book_id)
+
+    book.authors.add(author)
+
+    return redirect(f'books/{book_id}/display')
+
+
+def add_book(request):
+    print(request.POST)
+
+    author_id = request.POST['author_id']
+    book_id = request.POST['book']
+    author = Author.objects.get(id=author_id)
+    book = Book.objects.get(id=book_id)
+
+    author.books.add(book)
+
+    return redirect(f'authors/{author_id}/display')
+
+
+def authors(request):
+    context = {
+        'all_authors': Author.objects.all(),
+    }
+
+    return render(request, 'authors.html', context)
+
+
+def process_author(request):
+    print(request.POST)
+
+    Author.objects.create(
+        first_name = request.POST['author_first_name'],
+        last_name = request.POST['author_last_name'],
+        notes = request.POST['author_notes']
+    )
+
+    return redirect('/authors')
